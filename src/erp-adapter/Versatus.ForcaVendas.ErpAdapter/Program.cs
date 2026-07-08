dff6083f31f81c819d52387de7ccebc514576202
josemaramorim <amorimtrade@gmail.com>
2026-07-08 10:38:14 -0300

    fix: add missing using System.Reflection to Worker and ErpAdapter Program.cs

--- src/erp-adapter/Versatus.ForcaVendas.ErpAdapter/Program.cs
+++ src/erp-adapter/Versatus.ForcaVendas.ErpAdapter/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 using Versatus.ForcaVendas.ErpAdapter.Jobs;
 
 // Captura a versão embutida no assembly no momento da compilação
--- src/worker/Versatus.ForcaVendas.Worker/Program.cs
+++ src/worker/Versatus.ForcaVendas.Worker/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 using StackExchange.Redis;
 using Versatus.ForcaVendas.Infrastructure.Data;
 using Versatus.ForcaVendas.Worker.Jobs;
